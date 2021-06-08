79cb923e5c4ff20c7ff8fee826f1c81a0a56a4e5
Olaf Neumann <git-account@olafneumann.org>
2021-06-08 10:16:55 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("org.jetbrains.kotlin.js") version "1.5.10"
+}
+
+group = "org.olafneumann"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-js"))
+}
+
+kotlin {
+    js {
+        browser {
+            webpackTask {
+                cssSupport.enabled = true
+            }
+
+            runTask {
+                cssSupport.enabled = true
+            }
+
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    webpackConfig.cssSupport.enabled = true
+                }
+            }
+        }
+        binaries.executable()
+    }
+}
