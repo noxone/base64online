9ba9ff8cf46fbab42af8150bfc9b2e37ce500e0c
neumaol <neumaol@hlag.com>
2021-06-09 15:44:32 +0200

    Add file controls

--- src/main/kotlin/org/olafneumann/base64online/main.kt
+++ src/main/kotlin/org/olafneumann/base64online/main.kt
@@ -4,7 +4,13 @@ import kotlinx.browser.document
 import kotlinx.browser.window
 import org.olafneumann.base64online.base64.Base64
 import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.HTMLPreElement
 import org.w3c.dom.HTMLTextAreaElement
+import org.w3c.dom.events.Event
+import org.w3c.files.File
+import org.w3c.files.FileReader
+import org.w3c.files.get
+import org.w3c.xhr.ProgressEvent
 
 fun main() {
     window.onload = { initBase64Online() }
@@ -14,6 +20,9 @@ private fun initBase64Online() {
     val txtPlainInput: HTMLTextAreaElement = document.getElementById("b6-input-plain") as HTMLTextAreaElement
     val txtBase64Input: HTMLTextAreaElement = document.getElementById("b6-input-base64") as HTMLTextAreaElement
     val fileInput: HTMLInputElement = document.getElementById("b6-input-file") as HTMLInputElement
+    val fileOutput: HTMLPreElement = document.getElementById("b6-output-file") as HTMLPreElement
+    val fileActionEncode: HTMLInputElement = document.getElementById("b6-file-action-encode") as HTMLInputElement
+    val fileActionDecode: HTMLInputElement = document.getElementById("b6-file-action-encode") as HTMLInputElement
 
     txtPlainInput.oninput = {
         val text = txtPlainInput.value
@@ -25,8 +34,21 @@ private fun initBase64Online() {
         txtPlainInput.value = Base64.decode(base64)
         null
     }
-    fileInput.onchange = {
-        console.log(fileInput.value)
+    val fileChangeHandler: (Event) -> dynamic = {
+        fileInput.files?.get(0)?.read() { fileOutput.innerText = if (fileActionEncode.checked) {
+            Base64.encode(it)
+        } else {
+            Base64.decode(it)
+        } }
         null
     }
+    fileInput.onchange = fileChangeHandler
+    fileActionEncode.oninput = fileChangeHandler
+    fileActionDecode.oninput = fileChangeHandler
+}
+
+private fun File.read(callback: (String) -> Unit) {
+    val reader = FileReader()
+    reader.addEventListener("load", { callback(reader.result as String) })
+    reader.readAsBinaryString(this)
 }
